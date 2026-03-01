ecc9140c15d820a5cd25b8b048f9142c8808409a
Naveen Vootla <naveen.vootla@modulrfinance.com>
2026-03-01 15:28:02 +0000

    Simple chat endpoint that works with gemma3

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ollama-demo
+
+* Install ollama
+* Run gemma3 in your local `ollama run gemma3`
+* Start the application and hit the `/chat` endpoint with your question using the request param `message`
+* e.g, `client.http`
---
+++ src/test/java/com/modulr/ollama/demo/OllamaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modulr.ollama.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
